463404a08e03c733af200d0842b4948231d3e524
Tournesol02 <brage.johnsen79@gmail.com>
2025-12-12 08:55:51 +0100

    Test

---
+++ project_name/clock.cpp
@@ -0,0 +1,13 @@
+#include "Application.hpp"
+#include "clock.hpp"
+
+//Libraries déjà definies:
+#include <Wire.h>
+#include <RTClib.h>
+#include <LiquidCrystal_I2C.h>
+
+int set_alarm(int heures, int minutes) {
+
+
+  return 0;
+}
