20fb17fecb7ab6aac8086d93817922afcbcb88cd
Tournesol02 <brage.johnsen79@gmail.com>
2025-12-19 11:03:58 +0100

    clock.hpp is not compilable, needs further correcting

--- clock.hpp
+++ clock.hpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #ifndef _CLOCK_HPP
 #define _CLOCK_HPP
-using namespace std;
 
 //or #pragma once
 //Classes
@@ -11,43 +10,19 @@ class time{
   private:
   protected:
     enum class day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-    day day;
-    int hour;
-    int minute
-  public:
-    time SetTime(day d, hour h, minute m) : day(d), hour(h), minute(m){
-      
+    day d;
+    int h;
+    int m;
+    public:
+    time Time(day d, int h, int m) : day(d), hour(h), minute(m){
+      if(h<0 || h>23){std::cout<<"Hour needs be between 00:00 and 23:00"<<endl;}
+      if(m<0 || m>59){std::cout<<"Minutes needs be between 0 and 59"<<endl;}
     }
+    int GetHour(){return hour;}
+    int GetMinute(){return minute}
+    day GetDay(){return day;}
 };
  
-class hour : tim
-
-class time{
-private:
-protected:
-  enum class day{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
-    public:
-  };
-  class hour{
-    private:
-    protected:
-      explicit hour(int h = 0):value(h){
-        if (h<0 || h>23){throw std::out("Hour needs be between 0 and 23 !");}
-      }
-      int value;
-      public:
-      };
-  class minute{
-    private:
-    protected:
-      explicit minute(int m = 0):value(m){
-        if (m<0 || m>59){throw stdout<<"Minutes are to be between 0 and 59"<<endl;}
-       }
-       int value;
-    public:
-  };
-};
-
 #endif
 
 
