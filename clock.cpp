14323751278bf4f3ac195a2460954de4df09f3a5
oskarorvik <account.oskar@protonmail.com>
2025-12-21 22:39:39 +0100

    Dividing classes and managing heritage
    Compiles without errors. Moved functions from .hpp to .cpp in order to have only declarations in .hpp file.

--- clock.cpp
+++ clock.cpp
@@ -1,6 +1,41 @@
 #include "clock.hpp"
 
-int set_alarm(int heures, int minutes) { // For an alarm going off after x hours and y minutes
+time::time(){};
+int time::GetDay() {
+  return d;
+}
+int time::GetHour() {
+  return h;
+}
+int time::GetMinute() {
+  return m;
+}
+int time::begin(int day, int hour, int minute){
+  d = day;
+  h = hour;
+  m = minute;
+  if (h<0 || h>23 || m<0 || m>59){
+    return -1;
+  }
+  else{
+    return 0;
+  }
+}
+
+minute::minute(int m) {
+  if (m < 0 || m > 59) {
+    std::cout << "Minutes are to be between 0 and 59" << std::endl;
+  }
+};
+hour::hour(int h) {
+  if (h < 0 || h > 23) {
+    std::cout << "Hour needs be between 0 and 23 !" << std::endl;
+  }
+};
+
+
+
+int set_alarm(int heures, int minutes) {  // For an alarm going off after x hours and y minutes
   timer1_enabled();
 
 
--- clock.hpp
+++ clock.hpp
@@ -8,27 +8,45 @@
 #include <iostream>
 #include <Arduino.h>
 
-/*
+
 
 //Classes
 
 class time{
   private:
   protected:
-    enum class day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-    day d;
+    int d;
     int h;
     int m;
-    public:
-    time Time(day d, int h, int m) : day(d), hour(h), minute(m){
-      if(h<0 || h>23){std::cout<<"Hour needs be between 00:00 and 23:00"<<endl;}
-      if(m<0 || m>59){std::cout<<"Minutes needs be between 0 and 59"<<endl;}
-    }
-    int GetHour(){return hour;}
-    int GetMinute(){return minute}
-    day GetDay(){return day;}
+  public:
+    time();
+    int begin(int day, int hour, int minute);
+    int GetHour();
+    int GetMinute();
+    int GetDay();
 };
- 
+
+enum day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+class hour : time{
+  private:
+  protected:
+    hour(int h);
+    int value;
+    public:
+};
+
+class minute : time{
+  private:
+  protected:
+    minute(int m);
+    int value;
+  public:
+};
+
+
+
+/* 
 class hour : time
 
 class time{
@@ -37,30 +55,22 @@ protected:
   enum class day{Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
     public:
   };
-  class hour{
-    private:
-    protected:
-      explicit hour(int h = 0):value(h){
-        if (h<0 || h>23){throw std::out("Hour needs be between 0 and 23 !");}
-      }
-      int value;
-      public:
-      };
-  class minute{
-    private:
-    protected:
-      explicit minute(int m = 0):value(m){
-        if (m<0 || m>59){throw stdout<<"Minutes are to be between 0 and 59"<<endl;}
-       }
-       int value;
-    public:
-  };
+{
+    
+
+{
+      if (m<0 || m>59){std::cout << "Minutes are to be between 0 and 59" << std::endl;}
 
 };
+
+    time Time(day d, int h, int m) : day(d), hour(h), minute(m){
+      if(h<0 || h>23){std::cout<<"Hour needs be between 00:00 and 23:00"<<endl;}
+      if(m<0 || m>59){std::cout<<"Minutes needs be between 0 and 59"<<endl;}
+    }
 
 public:
 };
-*/
+/*
 class day{
 private:
 protected:
@@ -82,7 +92,7 @@ protected:
 public: 
 };
 
-
+*/
 #endif
 
 
